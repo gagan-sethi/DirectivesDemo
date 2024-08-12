bdc306ca61d0d048c8ff0446b831d2cd6354d984
Promatics Technologies Pvt Ltd, Ludhiana <gagandeep.sethi@promaticsindia.com>
2024-08-12 11:42:40 +0530

    Directives Demo

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NgClass, NgFor } from '@angular/common';
+
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [NgClass, NgFor],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+user="LPU"
+students:any =[
+  {
+    name:"Atul",
+    fees:5000,
+    studentImage:'https://picsum.photos/200/300'
+  },
+  {
+    name:"Rahul",
+    fees:0,
+    studentImage:'https://picsum.photos/200/300'
+  },
+  {
+    name:"Vijaya Lakshmi",
+    fees:10000,
+    studentImage:'https://picsum.photos/200/300'
+  }
+]
+
+}
